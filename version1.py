8ab12f8c1789270407e70d8a131cd74c09e09a7d
vanes11 <vanfokou@gmail.com>
2021-05-03 21:21:34 +0100

    verion stable de GeneratePosition excepté les cas de base.

--- version1.py
+++ version1.py
@@ -26,8 +26,6 @@ def GetClassC():
 
 ClasseC = GetClassC()
 
-# print(c['RightParenthesisTok'])
-
 
 def Cpos(s,k):
     if(k>= 0):
@@ -37,7 +35,6 @@ def Cpos(s,k):
 
 
 cpos = Cpos("laure", -1)
-# print(cpos)
 
 
 def TokenSeq(*tokens):
@@ -47,37 +44,43 @@ def TokenSeq(*tokens):
         TokenSeqResult = TokenSeqResult+token
     return TokenSeqResult
 
-# print(TokenSeq(ClasseC ['HyphenTok'], ClasseC ['EndTok']))
 
-#print(c['Bottom'])
 def Pos(s,r1,r2,c):
     """ 
         Retourne l'indice de debut du cieme matching de l'expression reguliere 
         TokenSeq(r1,r2)
     """
-    #print(len(s))
     r = TokenSeq(r1, r2)
+    
     r = re.compile(r)
+    r1 = re.compile(r1)
+    r2 = re.compile(r2)
     RegulationExpression = r.findall(s)
-   
+    
     if len(RegulationExpression) >= abs(c):
         e = abs(c)-1
         if c > 0:
             
             res = re.search(RegulationExpression[e],s)
-            t = res.start()
-           
+            res1 = re.search(r1,s)
+            Taille = res1.end() - res1.start() 
+            t = res.start() + Taille       
                 
         else:
-            RegulationExpression.reverse()
+            e = len(RegulationExpression) + c
             res = re.search(RegulationExpression[e],s)
-            t = res.start()
+            res1 = re.search(r1,s)
+            Taille = res1.end() - res1.start() 
+            t = res.start() + Taille
         return t
     else:
         return BOTTOM
 
-pos = Pos("22van 11laure12van3remeo11", ClasseC ['NumTok'],ClasseC ['AlphTok'],5)
-#print(pos)
+""" pos = Pos("425-706-7709", ClasseC['HyphenTok'],ClasseC['NumTok'],-2)
+pos1 = Pos("425-706-7709", ClasseC['NumTok'],ClasseC['HyphenTok'],-2)
+print(pos)
+print(pos1)
+ """
 
 
 def GenerateRegularExpressionLeft(s,k):
@@ -89,33 +92,32 @@ def GenerateRegularExpressionLeft(s,k):
     r1 = []
     Tokens = []
     while  k1 <= k-1 :
-        #print(k1)
         for cle in ClasseC:
             Token = ClasseC[cle]
             TokenComp = re.compile(Token)
             Test = TokenComp.match(s[k1:k])
             
             if Test != None:
-                # comment sortir du for, lorque Test
-                k1 = k1 + Test.end()-1
-                Tokens.append((k1,Token))
-                
+                k1 = k1 + Test.start()
+                Tokens.append((k1,Token,cle))
+                k1 =  k1 + Test.end()
+              
+                break
     Taille = len(Tokens)
     i = 0
-    #print(Tokens)
-    Tokens.reverse()
     while  i < Taille :
-        i = i +11
         Expression = "".join([str(elt[1]) for elt in Tokens[i: Taille]])
-        r1.append((Tokens[i][0],Expression))
+        Expression2 = ",".join([str(elt[2]) for elt in Tokens[i: Taille]])#donne l'ecriture sur forme de cle: Numtok, Hyphentok...
+        r1.append((Tokens[i][0],Expression,Tokens[i: Taille],Expression2))
+        i = i +1
     return r1   
 
 
 def GenerateRegularExpressionRigth(s,k):
-    """ 
-    Retourne l'ensemble des expressions regulieres qui existennt dans  s[k1:k] 
-    pour k1 variant de 0 a k-1
-    """
+    
+    """ Retourne l'ensemble des expressions regulieres qui existennt dans  s[k1:k] 
+    pour k1 variant de 0 a k-1 """
+   
     k1 = k
     r1 = []
     Tokens = []
@@ -125,32 +127,25 @@ def GenerateRegularExpressionRigth(s,k):
             Token = ClasseC[cle]
             TokenComp = re.compile(Token)
             Test = TokenComp.match(s[k1:Longueur])
-            if Test != None:
-            # comment sortir du for, lorque Test
-                k1 = k1 + Test.end()-1
-                """ print("valeur de k1:",k1)
-                print(Token) """
-                Tokens.append((k1,Token))
             
+            if Test != None:
+                k1 = k1 + Test.end()-1 # on doit nomalement faire end()-1, mais pour respecter l'indexation de python on prend end()
+                Tokens.append((k1,Token,cle))
+                k1 = k1 + 1
+                break
+
     Taille = len(Tokens)
     #print(Tokens)
-    Tokens.reverse()
     i = 0
     while  i < Taille:
+        Expression = "".join([str(elt[1]) for elt in Tokens[0: i+1]])
+        Expression2 = ",".join([str(elt[2]) for elt in Tokens[0: i+1]])
+        r1.append((Tokens[i][0],Expression,Tokens[0: i+1],Expression2))
         i = i +1
-        Expression = "".join([str(elt[1]) for elt in Tokens[i: Taille]])
-        r1.append((Tokens[i][0],Expression))
     return r1   
 
-
-
-
-test1 = GenerateRegularExpressionLeft("425-706-7709",5)
-print(test1)
-test = GenerateRegularExpressionRigth("425-706-7709",4)
-print(test)
-
-
+""" test = GenerateRegularExpressionRigth("425-706-7709",4)
+print(test) """
 
 
 def MatchExpression(l,s):
@@ -158,7 +153,7 @@ def MatchExpression(l,s):
     etant donnee une l, cette fonction retourne l'indice(l'occurence) de s 
     """
     try:
-        return l.index(s)
+        return l.index(s) + 1
 
     except ValueError:
         return 0
@@ -166,42 +161,46 @@ def MatchExpression(l,s):
 
 
 def GeneratePosition(s,k):
-    """  
-    retourne l'ensemble des differentes facons de representer une position
-    donnee dans une chaine donnee avec les primitives du langage 
-    """
+     
+    """ retourne l'ensemble des differentes facons de representer une position
+    donnee dans une chaine donnee avec les primitives du langage  """
+    POsList = []  # variable de formatage 
+    PosChain = ""
     result = set([Cpos(s,k), Cpos(s,-(len(s)-k))])
-
+    PosChain = "Cpos("+s+","+str(k)+")"
+    POsList.append(PosChain)
+    PosChain = "Cpos("+s+",-"+str((len(s)-k))+")"
+    POsList.append(PosChain)
+    #print(result)
     R1List = GenerateRegularExpressionLeft(s,k)
-    print(R1List)
-    print(R1List[1][1])
+    #print(R1List)
+    #print("****")
     R2List = GenerateRegularExpressionRigth(s,k)
-    print(R2List[1][1])
-    print(R2List)
+   
+    #print(R2List)
 
     for r1 in R1List :
         for r2 in R2List :
-            print("valeur de r1:",r1)
-            print("valeur de r2:",r2)
             r12 = TokenSeq(r1[1],r2[1])
-            print("valeur de r12:",r12)
-            r12 =  re.compile(r12)
+            r12 = re.compile(r12)
             MatchingList = r12.findall(s)
-            print(MatchingList)
-            print(r1)
-            print(r2)
-            print(r1[0])
-            c = MatchExpression(MatchingList,s[r1[0], r2[0]])
+            c = MatchExpression(MatchingList,s[r1[0]:r2[0]+1])
             c1 = len(MatchingList)
-            c1 = -(c1 -c +1)
-            result = result.union(set([Pos(s,r1[1],r2[1],c),Pos(s,r1[1],r2[1],c1)]))
-            
+            if c!=0 and (r1[2][len(r1[2])-1][1] != r2[2][0][1]):
+        
+                c1 = -(c1 -c +1)
+                PosChain = "Pos("+s+",TokenSeq("+r1[3]+"),TokenSeq("+r2[3]+"),"+str(c)+")"
+                POsList.append(PosChain)
+                PosChain = "Pos("+s+",TokenSeq("+r1[3]+"),TokenSeq("+r2[3]+"),"+str(c1)+")"
+                POsList.append(PosChain)
 
-    return 0
+                result = result.union(set([Pos(s,r1[1],r2[1],c),Pos(s,r1[1],r2[1],c1)]))
+
+    return result,set(POsList)
          
-#positions = GeneratePosition("425-706-7709",4)
-#print(positions)
-
+positions,poslist = GeneratePosition("425-706-7709",4)
+print(positions)
+print(poslist)
 
 
 
